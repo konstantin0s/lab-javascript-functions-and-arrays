35f315b71fc3e9dfe16afd04a2334e6002e333b9
konstantin0s <constantintofan85@gmail.com>
2019-01-29 12:10:31 +0100

    Add max function

---
+++ js/func.js
@@ -0,0 +1,27 @@
+
+
+//Find the maximum
+function maxOfTwoNumbers(a, b) {
+  if (a > b) {
+      return a;
+  } else if (a < b) {
+      return b;
+  } else {
+      console.log(a + " = " + b); } }
+
+console.log(maxOfTwoNumbers(5,5));
+
+var words = [
+  'mystery',
+  'brother',
+  'aviator',
+  'crocodile',
+  'pearl',
+  'orchard',
+  'crackpot'
+];
+
+//Finding the longest word
+function findLongestWord() {
+
+}
